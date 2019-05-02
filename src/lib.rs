883adfdde5a683fcd991c2531033faee210c9270
David Vo <david@vovo.id.au>
2019-05-02 15:31:41 +1000

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+pub mod ai_calibration;
+pub mod can_session_mux;
+pub mod frc_comm;
+pub mod usage_reporting;
---
+++ src/usage_reporting.rs
@@ -0,0 +1,15 @@
+pub use self::FRC_NetworkCommunication_nUsageReporting_report as report;
+
+use std::os::raw::c_char;
+
+extern "C" {
+    /**
+     * Report the usage of a resource of interest.
+     *
+     * @param resource one of the values in the tResourceType above (max value 51).
+     * @param instanceNumber an index that identifies the resource instance.
+     * @param context an optional additional context number for some cases (such as module number).  Set to 0 to omit.
+     * @param feature a string to be included describing features in use on a specific resource.  Setting the same resource more than once allows you to change the feature string.
+     */
+    pub fn FRC_NetworkCommunication_nUsageReporting_report(resource: u8, instanceNumber: u8, context: u8, feature: *const c_char) -> u32;
+}
